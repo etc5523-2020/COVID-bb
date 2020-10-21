0a3d04e75fe9d375404152b6de1b340f06ee2667
Yiwen-Zhang-259 <yzha0633@student.monash.edu>
2020-10-21 17:47:22 +0800

    create R script called “covid_19_all.R” and “covid-19_data.R”in the “data-raw” directory to generate the package data

---
+++ data-raw/covid_19_data/covid_19_data.R
@@ -0,0 +1,8 @@
+## code to prepare `covid_19_data` dataset goes here
+
+library(tidyverse)
+
+covid_19_data <- read_csv(here::here("data/covid_19_data.csv"))
+
+
+usethis::use_data(covid_19_data, overwrite = TRUE)
